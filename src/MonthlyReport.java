bbb739b06b542111f9a24d934215bcbee6aec81b
Anton Gubernatorov <gubernatorov.tosha@gmail.com>
2024-07-17 17:33:23 +0300

    create readMonthlyReports

--- src/MonthlyReport.java
+++ src/MonthlyReport.java
@@ -11,5 +11,42 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class MonthlyReport {
+    HashMap<Integer, ArrayList<MonthlyRecord>> monthlyRecords = new HashMap<>();
+    //Считывание
+    private void readMonthlyReports() throws IOException {
+        for (int month = 0; month < 12; month++){
+            ArrayList<MonthlyRecord> monthRecord = new ArrayList<>();
+            String path = "resources/m.20210" + month + ".csv";
+            if(Files.exists(Path.of(path))){
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+                String line = null;
+                int iteration = 0;
+                Scanner scanner = null;
+                while((line = bufferedReader.readLine()) != null){
+                    if(iteration == 0){
+                        iteration++;
+                        continue;
+                    }
+                    scanner = new Scanner(line);
+                    scanner.useDelimiter(",");
+                    while(scanner.hasNext()){
+                        String itemName = scanner.next();
+                        boolean isExpense = Boolean.parseBoolean(scanner.next());
+                        int quantity = Integer.parseInt(scanner.next());
+                        int sumOfOne = Integer.parseInt(scanner.next());
+                        MonthlyRecord monthlyRecord = new MonthlyRecord(itemName, isExpense, quantity, sumOfOne);
+                        monthRecord.add(monthlyRecord);
+                    }
+                }
+                monthlyRecords.put(month, monthRecord);
+            }
+        }
+        System.out.println(monthlyRecords);
+    }
+
+    //Определение самого прибыльного товара
+
+    //Определение самого убыточного товара
+    //Печать статистики
 
 }
